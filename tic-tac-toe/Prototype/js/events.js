386defb2b801a4b5415df615b8a25251bf46e40b
potato707 <63262993+potato707@users.noreply.github.com>
2022-10-19 11:31:58 +0200

    Tic-Tac-Toe Functionality

---
+++ tic-tac-toe/Prototype/js/events.js
@@ -0,0 +1,63 @@
+const grid = {"square-1": 0, "square-2": 0,"square-3": 0,"square-4": 0,"square-5": 0,"square-6": 0,"square-7": 0,"square-8": 0,"square-9": 0};
+let current_turn = 'x';
+
+window.addEventListener("load", main)
+
+const switch_turns = () =>{
+    if (current_turn==="x"){
+        current_turn="o";
+    } else {
+        current_turn="x";
+    }
+}
+
+const check_if_winner = (element_id) => {
+    square_number = parseInt(element_id.slice(element_id.length-1))
+    isVerticallyWinner()
+    isHorizontallyWinner()
+    isDiagnollyWinner()
+}
+
+const isVerticallyWinner = () => {
+    for (i=1; i<=9; i+=3){
+        if (grid["square-"+(i.toString())] && grid["square-"+(i.toString())] === grid["square-"+((i+1).toString())] && grid["square-"+((i+1).toString())] === grid["square-"+((i+2).toString())]){
+            console.log(grid["square-"+(i.toString())] + " is winner")
+        }
+    }
+}
+
+const isHorizontallyWinner = () => {
+    for (i=1; i<=3; i++){
+        if (grid["square-"+(i.toString())] && grid["square-"+(i.toString())] === grid["square-"+((i+3).toString())] && grid["square-"+((i+3).toString())] === grid["square-"+((i+6).toString())]){
+            console.log(grid["square-"+(i.toString())] + " is winner")
+        }
+    }
+}
+
+const isDiagnollyWinner = () => {
+    if (grid["square-5"]){
+        if (grid["square-1"] === grid["square-5"] && grid["square-5"] === grid["square-9"] || grid["square-3"] === grid["square-5"] && grid["square-5"] === grid["square-7"]){
+            console.log(grid["square-5"] + " is winner")
+        }
+    }
+}
+
+const printSquare = (element) => {
+    let element_id = element.srcElement.id
+    if (!grid[element_id]){
+        grid[element_id] = current_turn;
+        document.getElementById(element_id).innerHTML=current_turn
+        check_if_winner(element_id)
+        switch_turns()
+    }
+    console.log(grid);
+}
+
+const add_listener = (element) => {
+    element.addEventListener("click", printSquare);
+}
+
+function main(){
+    const squares_elements = [...document.getElementsByClassName("square")];
+    squares_elements.forEach(add_listener);
+}
